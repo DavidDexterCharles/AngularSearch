ba2b8c57c9afb6f898809e77b73594af57528338
David Charles <david.dexter.charles@gmail.com>
2016-02-01 13:59:34 -0400

    test with 1000 records

--- ang.php
+++ ang.php
@@ -6,4 +6,10 @@ require_once('Jengine.php');
 
 
 $template=Jengine::json_template('http://localhost/AngularSearch/data.json');
+$json_data2=array("David", "Cows", "grey","http://www.gettyimages.com/gi-resources/images/CreativeImages/Hero-527920799.jpg");
+
+for ($x = 0; $x <= 1000; $x++) {
+    $template=Jengine::json_add($template,$json_data2);
+}
+
 echo $template;
